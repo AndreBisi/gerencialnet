291e01c7246d766ea7a05ce3b6fe85d71f22053e
AndreBisi <andregbisi@gmail.com>
2023-06-18 20:11:43 -0300

    atu 18/06/2023 2

--- src/main/java/br/com/gerencialnet/domain/conexao/DetalhamentoConexaoDTO.java
+++ src/main/java/br/com/gerencialnet/domain/conexao/DetalhamentoConexaoDTO.java
@@ -2,10 +2,8 @@ package br.com.gerencialnet.domain.conexao;
 
 public record DetalhamentoConexaoDTO(Long id, String descricao) {
 	
-	public DetalhamentoConexaoDTO(Conexao conexao) {
-		
-		this( conexao.getId(), conexao.getDescricao() );
-		
+	public DetalhamentoConexaoDTO(Conexao conexao) {		
+		this( conexao.getId(), conexao.getDescricao() );		
 	}
 
 }
--- src/main/java/br/com/gerencialnet/domain/conexao/ListagemConexaoDTO.java
+++ src/main/java/br/com/gerencialnet/domain/conexao/ListagemConexaoDTO.java
@@ -2,10 +2,8 @@ package br.com.gerencialnet.domain.conexao;
 
 public record ListagemConexaoDTO(Long id, String descricao) {
 	
-	public ListagemConexaoDTO(Conexao conexao) {
-		
-		this( conexao.getId(), conexao.getDescricao() );
-		
+	public ListagemConexaoDTO(Conexao conexao) {		
+		this( conexao.getId(), conexao.getDescricao() );		
 	}
 
 }
